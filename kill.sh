f04d0b1845d058f33df05f81da3cba4d4bfb4a7c
fw05 <jhjo1997@naver.com>
2022-10-26 00:05:54 +0900

    fc

---
+++ kill.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+source /home/fw05/.zshrc
+
+echo "Launching Application, please wait!"
+
+gnome-terminal --geometry=40x40  \
+--tab --title="roskill" -e "zsh -c \"rosnode kill -a;exec zsh\"" \
+--tab --title="kill terminal" -e "zsh -c \"killall -3 gnome-shell;exec zsh\""
---
+++ launch_ui.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env zsh
+
+echo "Launching UI, please wait!"
+
+python -m webbrowser http://192.168.0.13:8001/
