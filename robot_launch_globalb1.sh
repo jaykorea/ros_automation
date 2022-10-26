a281a25d502ade463413c1b8f031bb73515f434b
fw <jhjo1997@naver.com>
2022-10-27 00:08:52 +0900

    param modified3

--- robot_launch_globalb1.sh
+++ robot_launch_globalb1.sh
@@ -4,10 +4,19 @@ source /home/fw/.zshrc
 
 echo "Launching Application, please wait!"
 
-terminator --new-tab -T "pm2" -e "zsh -c \"pm2 restart 1;exec zsh\"" &
-terminator --new-tab -T "roscore" -e "zsh -c \"roscore;exec zsh\"" &
-terminator --new-tab -T "init" -e "zsh -c  \"sleep 5s;roslaunch fw_rev_04_init fw_robot.launch;exec zsh\"" &
-terminator --new-tab -T "cam" -e "zsh -c \"sleep 5s;roslaunch fw_rev_04_cam launch_d455_ekf_wheel_imu_jay.launch initial_reset:=false;exec zsh\"" &
-terminator --new-tab -T "slam" -e "zsh -c \"sleep 5s;roslaunch fw_rev_04_rtabmap fw_navigation_stereo_test2_jay_2dlidar.launch rviz:=false database_path:=~/.ros/globalb1.db;exec zsh\"" &
-terminator --new-tab -T "nav" -e "zsh -c \"sleep 5s;roslaunch fw_rev_04_navigation fw_move_base_teb.launch;exec zsh\"" &
+#terminator --new-tab -T "pm2" -e "zsh -c \"pm2 restart 1;exec zsh\"" &
+#terminator --new-tab -T "roscore" -e "zsh -c \"source /home/fw/.zshrc;roscore;exec zsh\"" &
+#terminator --new-tab -T "init" -e "zsh -c  \"source /home/fw/.zshrc;sleep 5s;roslaunch fw_rev_04_init fw_robot.launch;exec zsh\"" &
+#terminator --new-tab -T "cam" -e "zsh -c \"source /home/fw/.zshrc;sleep 5s;roslaunch fw_rev_04_cam launch_d455_ekf_wheel_imu_jay.launch initial_reset:=false;exec zsh\"" &
+#terminator --new-tab -T "slam" -e "zsh -c \"source /home/fw/.zshrc;sleep 5s;roslaunch fw_rev_04_rtabmap fw_navigation_stereo_test2_jay_2dlidar.launch rviz:=false database_path:=~/.ros/globalf4.db;exec zsh\"" &
+#terminator --new-tab -T "nav" -e "zsh -c \"source /home/fw/.zshrc;sleep 5s;roslaunch fw_rev_04_navigation fw_move_base_teb.launch;exec zsh\"" &
+
+
+gnome-terminal --geometry=40x40  \
+--tab --title="pm2" -e "zsh -c \"pm2 restart 1;exec zsh\"" \
+--tab --title="roscore" -e "zsh -c \"source /home/fw/.zshrc;roscore;exec zsh\"" \
+--tab --title="init" -e "zsh -c  \"source /home/fw/.zshrc;sleep 5s;roslaunch fw_rev_04_init fw_robot.launch;exec zsh\"" \
+--tab --title="cam" -e "zsh -c \"source /home/fw/.zshrc;sleep 5s;roslaunch fw_rev_04_cam launch_d455_ekf_wheel_imu_jay.launch initial_reset:=false;exec zsh\"" \
+--tab --title="slam" -e "zsh -c \"source /home/fw/.zshrc;sleep 5s;roslaunch fw_rev_04_rtabmap fw_navigation_stereo_test2_jay_2dlidar.launch rviz:=false database_path:=~/.ros/globalb1.db;exec zsh\"" \
+--tab --title="nav" -e "zsh -c \"source /home/fw/.zshrc;sleep 5s;roslaunch fw_rev_04_navigation fw_move_base_teb.launch;exec zsh\""
 
